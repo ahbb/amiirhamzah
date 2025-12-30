aabb1021d536d2f2bec6455a73dde1fdd6b15601
Amiir Hamzah <amiir9955@gmail.com>
2025-12-31 01:53:53 +0800

    about section
    - add fastapi
    - some styling

--- app/components/Portfolio.jsx
+++ app/components/Portfolio.jsx
@@ -1,17 +1,17 @@
 import { FaFlutter } from "react-icons/fa6";
-import { SiPython, SiNextdotjs, SiMongodb, SiMysql } from 'react-icons/si';
+import { SiFastapi, SiNextdotjs, SiMongodb, SiMysql } from 'react-icons/si';
 import { BiLogoSpringBoot } from "react-icons/bi";
 import { DiDjango } from "react-icons/di";
 
 export default function Portfolio() {
     return (
         // main div to return
-        <div className="max-w-full flex-1 flex flex-col items-center justify-between py-16 sm:py-32 px-4 sm:px-8 md:px-72">
+        <div className="max-w-full flex-1 flex flex-col items-center justify-between py-16 px-4 sm:py-32 sm:px-8 md:px-96">
             {/* About section */}
             <div className="w-full">
                 <section id="about" className="min-h-screen py-10 scroll-mt-20">
                     <h2 className="text-4xl sm:text-5xl font-bold mb-4 text-white">About</h2>
-                    <div className="flex flex-col lg:flex-row items-start gap-6 lg:gap-12">
+                    <div className="flex flex-col lg:flex-row items-start gap-6 lg:gap-16">
                         <div className="pt-3">
                             <p className="text-gray-300 leading-relaxed text-xl">
                                 Hi, I’m Amiir Hamzah, a technology enthusiast driven by a passion for building applications that create meaningful and positive impact. I enjoy developing digital solutions that simplify everyday processes, improve efficiency, and enhance the way people interact with technology.
@@ -22,13 +22,13 @@ export default function Portfolio() {
                         </div>
 
                         {/* Icons column (right side) */}
-                        <div className="flex flex-col gap-5 w-full lg:pl-36">
-                            <p className="text-gray-300">Some technologies I am working with:</p>
-                            <div className="flex flex-wrap items-center gap-4 text-center">
+                        <div className="flex flex-col gap-5 w-full">
+                            <p className="text-gray-300 text-center">Some technologies I am working with:</p>
+                            <div className="flex flex-wrap items-center gap-12 text-center justify-center">
                                 <a className="text-yellow-300 hover:text-white transition">
                                     <DiDjango className="w-10 h-10 sm:w-16 sm:h-16 md:w-20 md:h-20" />
                                 </a>
-                                <a className="text-gray-300 hover:text-white transition">
+                                <a className="hover:text-white transition">
                                     <SiNextdotjs className="w-10 h-10 sm:w-12 sm:h-12 md:w-12 md:h-12" />
                                     <p className="text-sm mt-2">Next.js</p>
                                 </a>
@@ -37,8 +37,8 @@ export default function Portfolio() {
                                     <p className="text-sm mt-2">Flutter</p>
                                 </a>
                             </div>
-                            <div className="flex flex-wrap items-center gap-8 text-center">
-                                <a className="text-green-400 hover:text-white transition">
+                            <div className="flex flex-wrap items-center gap-12 text-center justify-center">
+                                <a className="hover:text-white transition">
                                     <SiMongodb className="w-10 h-10 sm:w-12 sm:h-12 md:w-12 md:h-12" />
                                     <p className="text-sm mt-2">MongoDB</p>
                                 </a>
@@ -47,7 +47,13 @@ export default function Portfolio() {
                                 </a>
                                 <a className="hover:text-white transition">
                                     <BiLogoSpringBoot className="w-10 h-10 sm:w-12 sm:h-12 md:w-12 md:h-12" />
-                                    <p className="text-sm mt-2">Spring Boot</p>
+                                    <p className="text-sm mt-2">Spring <br/>Boot</p>
+                                </a>
+                            </div>
+                             <div className="flex flex-wrap items-center text-center justify-center">
+                                <a className="hover:text-white transition">
+                                    <SiFastapi className="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14" />
+                                    <p className="text-sm mt-2">Fast API</p>
                                 </a>
                             </div>
                         </div>
